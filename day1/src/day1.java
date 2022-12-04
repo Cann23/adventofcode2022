79dc24ec42fa3f906c88f2f28bcc870b41cac9ae
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-04 21:48:02 +0300

    reading input file

--- day1/src/day1.java
+++ day1/src/day1.java
@@ -7,11 +7,11 @@ public class day1 {
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Integer> caloriesList = new ArrayList<>();
 
-        File file = new File("input.txt");
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day1/src/input.txt");
         Scanner scanner = new Scanner(file);
 
-        while(scanner.hasNextLine()){
-            int a = scanner.nextInt();
+        while(scanner.hasNextInt()){
+            Integer a = scanner.nextInt();
             caloriesList.add(a);
         }
 

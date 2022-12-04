6e31b26c76b610ea4a91cd39095f47bf317062d8
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-04 22:00:37 +0300

    correct answer

--- day1/src/day1.java
+++ day1/src/day1.java
@@ -1,20 +1,31 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class day1 {
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<Integer> caloriesList = new ArrayList<>();
 
-        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day1/src/input.txt");
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day1/src/input2.txt");
         Scanner scanner = new Scanner(file);
+        int total = 0;
 
-        while(scanner.hasNextInt()){
-            Integer a = scanner.nextInt();
-            caloriesList.add(a);
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+
+            if(line.equals("")){
+                caloriesList.add(total);
+                total = 0;
+            }
+            else{
+                total += Integer.parseInt(line);
+            }
+
         }
 
-        System.out.println(caloriesList);
+        System.out.println(caloriesList.stream().collect(Collectors.summarizingInt(Integer::intValue)).getMax());
     }
 }

0555abcd4e24e2b7dff4928b7fea020eb0829591
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-08 21:00:32 +0300

    answer for part2.

--- day4/src/CampCleanup.java
+++ day4/src/CampCleanup.java
@@ -8,6 +8,7 @@ public class CampCleanup {
         Scanner scanner = new Scanner(file);
 
         int fullyContainNumber = 0;
+        int fullyContainNumber2 = 0;
 
         while(scanner.hasNext()){
             String nextLine = scanner.next();
@@ -15,6 +16,7 @@ public class CampCleanup {
             String[] firstElementOfPairs = pairs[0].split("-");
             String[] secondElementOfPairs = pairs[1].split("-");
 
+            // part1
             if( (Integer.parseInt(firstElementOfPairs[0]) >= Integer.parseInt(secondElementOfPairs[0])
                     && Integer.parseInt(firstElementOfPairs[1]) <= Integer.parseInt(secondElementOfPairs[1]))
                     || (Integer.parseInt(firstElementOfPairs[0]) <= Integer.parseInt(secondElementOfPairs[0])
@@ -22,8 +24,15 @@ public class CampCleanup {
                             fullyContainNumber++;
             }
 
+            // part2
+            if((Integer.parseInt(firstElementOfPairs[1]) >= Integer.parseInt(secondElementOfPairs[0]))
+                     ){
+                fullyContainNumber2++;
+            }
+
 
         }
         System.out.println(fullyContainNumber);
+        System.out.println(fullyContainNumber2);
     }
 }

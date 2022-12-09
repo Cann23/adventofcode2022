b5452dd761694ec1383da773ad44e0e82e783bf5
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-09 20:26:45 +0300

    initial commit

---
+++ day5/src/SupplyStacks.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SupplyStacks {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day5/src/input.txt");
+        Scanner scanner = new Scanner(file);
+
+        System.out.println("hello world");
+    }
+}

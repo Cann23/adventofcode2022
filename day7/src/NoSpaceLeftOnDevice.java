769caeb5bf7dd24a3f7a6a4995d1e53790e50416
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-13 19:13:33 +0300

    not completed

--- day7/src/NoSpaceLeftOnDevice.java
+++ day7/src/NoSpaceLeftOnDevice.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class NoSpaceLeftOnDevice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day7/src/input.txt");
+        Scanner scanner = new Scanner(file);
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        ArrayList<Integer> fileSize = new ArrayList<>();
+
+        while(scanner.hasNext()){
+            stringArrayList.add(scanner.nextLine());
+        }
+
+
+
+
         System.out.println("Hello World!");
     }
 }

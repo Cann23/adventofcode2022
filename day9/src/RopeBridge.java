3203898fd6bf3959db5c88a519b9ce3e659f6233
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-17 12:24:13 +0300

    the right answer for part 1

--- day9/src/RopeBridge.java
+++ day9/src/RopeBridge.java
@@ -7,10 +7,102 @@ import java.util.Set;
 
 public class RopeBridge {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day9/src/input.txt");
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day9/src/input2.txt");
         Scanner scanner = new Scanner(file);
 
+        Set<String> path = new HashSet<>();
+        //HashMap<Character, Integer> commands = new HashMap<>();
 
+        int verticalT = 0;
+        int horizontalT = 0;
+        int verticalH = 0;
+        int horizontalH = 0;
+        String horizontalLocation = "x";
+        String verticalLocation = "y";
+
+
+        // initial point. x and y
+        path.add(horizontalLocation + horizontalT + verticalLocation + verticalT);
+
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            String[] command = line.split(" ");
+            //commands.put(command[0].charAt(0), Integer.parseInt(command[1]));
+
+            for(int i = 0; i < Integer.parseInt(command[1]); i++){
+                switch (command[0].charAt(0)){
+                    case 'R':
+                        horizontalH++;
+                        if(horizontalH > horizontalT + 1){
+                            if(verticalH == verticalT){
+                                horizontalT++;
+                            }
+                            else if(verticalH > verticalT){
+                                horizontalT++;
+                                verticalT++;
+                            }
+                            else if(verticalH < verticalT){
+                                horizontalT++;
+                                verticalT--;
+                            }
+                        }
+                        break;
+                    case 'L':
+                        horizontalH--;
+                        if(horizontalH < horizontalT - 1){
+                            if(verticalH == verticalT){
+                                horizontalT--;
+                            }
+                            else if(verticalH > verticalT){
+                                horizontalT--;
+                                verticalT++;
+                            }
+                            else if(verticalH < verticalT){
+                                horizontalT--;
+                                verticalT--;
+                            }
+                        }
+                        break;
+                    case 'U':
+                        verticalH++;
+                        if(verticalH > verticalT + 1){
+                            if(horizontalH == horizontalT){
+                                verticalT++;
+                            }
+                            else if(horizontalH > horizontalT){
+                                verticalT++;
+                                horizontalT++;
+                            }
+                            else if(horizontalH < horizontalT){
+                                verticalT++;
+                                horizontalT--;
+                            }
+                        }
+                        break;
+                    case 'D':
+                        verticalH--;
+                        if(verticalH < verticalT - 1){
+                            if(horizontalH == horizontalT){
+                                verticalT--;
+                            }
+                            else if(horizontalH > horizontalT){
+                                verticalT--;
+                                horizontalT++;
+                            }
+                            else if(horizontalH < horizontalT){
+                                verticalT--;
+                                horizontalT--;
+                            }
+                        }
+                        break;
+                }
+                path.add(horizontalLocation + horizontalT + verticalLocation + verticalT);
+            }
+
+            System.out.println(path);
+        }
+
+        System.out.println(path.size());
 
     }
 }

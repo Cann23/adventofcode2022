d94e3548d605dec473cc52fb5392a13ef4c535ca
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-17 11:29:31 +0300

    initial commit

---
+++ day9/src/RopeBridge.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RopeBridge {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day9/src/input.txt");
+        Scanner scanner = new Scanner(file);
+
+
+
+    }
+}

5f97b91045175fd3cfb3fad048fb6387f21a9366
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-19 19:07:43 +0300

    initial commit

---
+++ day11/src/MonkeyInTheMıddle.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MonkeyInTheMıddle {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day10/src/input2.txt");
+        Scanner scanner = new Scanner(file);
+        System.out.println("monkey");
+    }
+}

a5202c11cb56bbcc5cff5ed745a3d0e59d10df0c
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-19 19:44:05 +0300

    monkey class is created

--- day11/src/MonkeyInTheMıddle.java
+++ day11/src/MonkeyInTheMıddle.java
@@ -1,11 +1,49 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+class Monkey{
+    private String number;
+    private String emptyLine;
+    private String startingItems;
+    private String operation;
+    private String test;
+    private String ifTrue;
+    private String ifFalse;
+
+    public Monkey(String number, String emptyLine
+            , String startingItems, String operation
+            , String test, String ifTrue, String ifFalse) {
+        this.number = number;
+        this.emptyLine = emptyLine;
+        this.startingItems = startingItems;
+        this.operation = operation;
+        this.test = test;
+        this.ifTrue = ifTrue;
+        this.ifFalse = ifFalse;
+    }
+}
 
 public class MonkeyInTheMıddle {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day10/src/input2.txt");
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day11/src/input.txt");
         Scanner scanner = new Scanner(file);
+
+        ArrayList<Monkey> monkeys = new ArrayList<>();
+
+        while (scanner.hasNext()){
+            Monkey monkey = new Monkey(scanner.next() + scanner.next()
+                                        , scanner.nextLine()
+                                        , scanner.nextLine()
+                                        , scanner.nextLine()
+                                        , scanner.nextLine()
+                                        , scanner.nextLine()
+                                        , scanner.nextLine());
+
+            monkeys.add(monkey);
+        }
+
         System.out.println("monkey");
     }
 }

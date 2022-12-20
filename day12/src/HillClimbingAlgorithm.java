343d5eaff17b04433dcbd9fed552fdc5f679c7c5
e2309755 <e2309755@ceng.metu.edu.tr>
2022-12-20 19:12:21 +0300

    creating and filling grid

--- day12/src/HillClimbingAlgorithm.java
+++ day12/src/HillClimbingAlgorithm.java
@@ -1,2 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HillClimbingAlgorithm {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/home/e2309755/Intellij/adventofcode/adventofcode/day12/src/input.txt");
+        Scanner scanner = new Scanner(file);
+
+        ArrayList<String> lines = new ArrayList<>();
+
+        // reading input
+        while (scanner.hasNext()){
+            String line = scanner.next();
+            lines.add(line);
+        }
+
+        // creating grid
+        char[][] grid = new char[lines.size()][lines.get(0).length()];
+
+        // filling grid
+        for(int i = 0; i < lines.size(); i++){
+            for(int j = 0; j < lines.get(i).length(); j++){
+                grid[i][j] = lines.get(i).charAt(j);
+            }
+        }
+
+        System.out.println('a' + 1 == 'b');
+    }
 }
